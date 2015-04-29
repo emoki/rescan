16a7ed9520e9e1efe6980db087637ceaf6c3edc1
ethan steffens <ethan.steffens@gmail.com>
2015-04-29 12:10:25 -0400

    Added Meta.cs.  This class will represent the meta data.  TBD.

---
+++ src/REScan.Data/Meta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace REScan.Data {
+    public class Meta {
+        public Meta(string fileName) {
+            Filename = Path.GetFileName(fileName);
+        }
+        public string Filename;
+    }
+}
