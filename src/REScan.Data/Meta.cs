1c43a2b48385bbf80444029b2e86a2a31249cd06
Ethan Steffens <ethan.steffens@gmail.com>
2024-10-19 09:42:05 -0400

    Add support for isHighBand
    This is used to set frequency for the Pctel data. It's more of a
    placeholder to distinguish differences during analysis.

--- src/REScan.Data/Meta.cs
+++ src/REScan.Data/Meta.cs
@@ -7,9 +7,12 @@ using System.IO;
 
 namespace REScan.Data {
     public class Meta {
-        public Meta(string fileName) {
+        public Meta(string fileName, bool isHighBand = false)
+        {
             Filename = Path.GetFileName(fileName);
+            this.isHighBand = isHighBand;
         }
         public string Filename;
+        public bool isHighBand;
     }
 }
